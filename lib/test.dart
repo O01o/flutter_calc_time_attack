db94488b9e3994bdb8d7c86f51dea6e5d5cb70a9
AyatoMatsui <77559448+O01o@users.noreply.github.com>
2022-12-17 13:26:41 +0900

    transfered json_list_decoder to data directory

--- lib/test.dart
+++ lib/test.dart
@@ -1,30 +1,15 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:flutter_calc_time_attack/data/json_list_decoder.dart';
+
 // import 'package:flutter_calc_time_attack/model/issue_data.dart';
 
 void main() {
   print("Hello world!!");
-  String path = "./data/issue_data.json";
-  if (FileSystemEntity.isDirectorySync(path)) {
-    print("path is directory");
-  } else {
-    final issueData = File(path);
-    String issueDataString = issueData.readAsStringSync();
-    List issueDataList = issueDataString.split("\n");
-    int index = 0;
-    issueDataList.forEach((data) {
-      data = data.trim();
-      if (index != 0 && index != issueDataList.length - 1) {
-        int dataLength = data.length - 1;
-        if (data[dataLength] != "}") {
-          data = data.toString().substring(0, dataLength);
-        } else {
-          data = data.toString();
-        }
-        data = json.decode(data);
-        print(data["issue"]);
-      }
-      index++;
-    });
-  }
+  List<Map<String, dynamic>> jsonListMap = jsonListDecoder(issueDataPath);
+  int jsonListMapLength = jsonListMap.length;
+  print("len: $jsonListMapLength");
+  jsonListMap.forEach((element) {
+    print(element["issue"]);
+  });
 }
